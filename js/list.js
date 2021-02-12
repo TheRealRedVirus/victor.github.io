3b8160be2a68cb37cb2174f5dc4434d592494e11
TheRealRedVirus <78984149+TheRealRedVirus@users.noreply.github.com>
2021-02-12 16:37:28 -0500

    Update list.js

--- js/list.js
+++ js/list.js
@@ -8,18 +8,34 @@ const d = {
 					"link": "https://youtu.be/IVUCnVHBPc8",
 					"percent": 100,
 					"hz": "288hz"
-				},{
+				},
+			"name": "Rigid Success",
+                       "author": "Sopar And TheRedVirus",
+                       "more": "none",
+                       "id": 64360108,
+                       "pass": "Free to copy",
+                       "percentToQualify": 20,
+                       "verificationVid": "https://www.youtube.com/watch?v=OSlBFtnbiXg",
+                       "key": 0
+                [
+		/*=================================================================================*/
+		{
+[
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
 					"user": "MichaelReeves",
 					"link": "https://youtu.be/IVUCnVHBPc8",
 					"percent": 100,
 					"hz": "288hz"
 				},
-			"name": "Shitty Cold Sweat",
-                       "author": "MP3141",
+			"name": "Bufa",
+                       "author": "???",
                        "more": "none",
                        "id": 64360108,
                        "pass": "Free to copy",
-                       "percentToQualify": 60,
+                       "percentToQualify": 20,
                        "verificationVid": "https://www.youtube.com/watch?v=OSlBFtnbiXg",
                        "key": 0
                 [
