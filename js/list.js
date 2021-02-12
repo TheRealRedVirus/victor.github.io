30449fbd85b62a36ffb2f95e6d36bc74dfab84e4
TheRealRedVirus <78984149+TheRealRedVirus@users.noreply.github.com>
2021-02-12 16:34:16 -0500

    Update list.js

--- js/list.js
+++ js/list.js
@@ -8,116 +8,12 @@ const d = {
 					"link": "https://youtu.be/IVUCnVHBPc8",
 					"percent": 100,
 					"hz": "288hz"
-				},
-				{
-					"user": "Lupe1204",
-					"link": "https://youtu.be/x3AKojLgNtE",
-					"percent": 100,
-					"hz": "Mobile"
-				},
-				{
-					"user": "Ryclix",
-					"link": "https://www.youtube.com/watch?v=0rRMy_5mS-M&feature=youtu.be&t=1414s",
+				},{
+					"user": "MichaelReeves",
+					"link": "https://youtu.be/IVUCnVHBPc8",
 					"percent": 100,
 					"hz": "288hz"
 				},
-				{
-				       "user": "Kapinapi",
-				       "link": "https://www.youtube.com/watch?v=p05WatZdcWE&feature=youtu.be",
-				       "percent": 100,
-				       "hz": "144hz"
-			       },
-				{
-					"user": "Vink",
-					"link": "https://youtu.be/sme9ZVKhU8I",
-					"percent": 64,
-					"hz": "120hz"
-				},
-				 {
-				       "user": "HideriGH",
-				       "link": "https://youtu.be/lCHvAzhX7ME",
-				       "percent": 61,
-				       "hz": "144hz"
-			       },
-				{
-				       "user": "Matter",
-				       "link": "https://youtu.be/qtoYAUH7TDg",
-				       "percent": 100,
-				       "hz": "144hz"
-			       },
-			       {
-				       "user": "Lelzi",
-				       "link": "https://youtu.be/-A37EZKjaXI",
-				       "percent": 100,
-				       "hz": "144hz"
-			       },
-			       {
-				       "user": "GregoryHouse",
-				       "link": "https://youtu.be/vGSgiHqBfi8",
-				       "percent": 100,
-				       "hz": "144hz"
-			       },
-				{
-				       "user": "Lawnmower",
-				       "link": "https://youtu.be/0INwjCY5oUQ",
-				       "percent": 79,
-				       "hz": "288hz"
-			       },
-				{
-					"user": "Sazix",
-					"link": "https://www.youtube.com/watch?v=Qa4EvYZ-DS8&feature=youtu.be",
-					"percent": 100,
-					"hz": "240hz"
-				},
-				{
-					"user": "aet",
-					"link": "https://youtu.be/xDroG1VxAc8",
-					"percent": 100,
-					"hz": "240hz"
-				},
-				{
-					"user": "Tai0",
-                                        "link": "https://youtu.be/suaHu4eUu-s",
-                                        "percent": 100,
-                                        "hz": "120hz"
-				},
-				{
-					"user": "Kobra456",
-                                        "link": "https://www.youtube.com/watch?v=ky4dFDIuYfU",
-                                        "percent": 100,
-                                        "hz": "144hz"
-				},
-				{
-					"user": "Magma",
-                                        "link": "https://youtu.be/8S0p4jTpEAs",
-                                        "percent": 100,
-                                        "hz": "240hz"
-				},
-				{
-					"user": "geothermalguy",
-                                        "link": "https://www.youtube.com/watch?v=ZL-D9Pb4RdY&feature=youtu.be",
-                                        "percent": 64,
-                                        "hz": "240hz"
-				},
-				{
-					"user": "Amza",
-                                        "link": "https://www.youtube.com/watch?v=5q2zcFpZUno",
-                                        "percent": 100,
-                                        "hz": "300hz"
-				},
-				{
-					"user": "BensonGD",
-                                        "link": "https://youtu.be/zkwruE4Rz4I",
-                                        "percent": 100,
-                                        "hz": "300hz"
-				},
-				{
-					"user": "Peachy",
-                                        "link": "https://youtu.be/BBVfNZuuDnE",
-                                        "percent": 100,
-                                        "hz": "144hz"
-				},
-			],
 			"name": "Shitty Cold Sweat",
                        "author": "MP3141",
                        "more": "none",
@@ -126,3 +22,6 @@ const d = {
                        "percentToQualify": 60,
                        "verificationVid": "https://www.youtube.com/watch?v=OSlBFtnbiXg",
                        "key": 0
+                [
+		/*=================================================================================*/
+		{
